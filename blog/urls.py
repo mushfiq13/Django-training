c560d31915fb511baea603e84aed1496d7e2fcd8
Mushfiqur Rahman <rahman.mushfiqur@enosisbd.com>
2022-07-27 11:54:20 +0600

    Fix urls, views.

--- blog/urls.py
+++ blog/urls.py
@@ -4,20 +4,12 @@ from .views import (
     blog_post_detail_view,
     blog_post_list_view,
     blog_post_create_view,
-    about_page,
 )
 
 urlpatterns = [
     path('', blog_post_list_view),
-    re_path(r'^room/$', views.room),
-    # path('room/', room),
-    path('example/', views.example_page),
-    path('create/', blog_post_create_view),
-    path('about/', views.about_page),
-    path('contact/', views.contact_page),
-
+    
     # path('detail/', blog_post_detail_page),
-
     # dynamic urls based
     # path('detail/<int:post_id>/', blog_post_detail_page),
     # re_path(r'^detail/(?P<post_id>\d+)/$', blog_post_detail_page),
@@ -25,4 +17,5 @@ urlpatterns = [
     # If we slug then we should follow this format:
     path('<str:slug>/', blog_post_detail_view),
     # re_path(r'^(?P<slug>\w+)/$', blog_post_detail_page),
+     path('create/', views.blog_post_create_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,37 +5,6 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
-from django.template.loader import get_template
-
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design Web Page'},
-]
-
-def home(request):
-    # return HttpResponse('Home')
-    context = {'rooms': rooms}
-    return render(request, 'home.html', context)
-
-def room(request):
-    something_here = "Room"
-    return HttpResponse(something_here)
-
-def about_page(request):
-    return HttpResponse('<h1>About Page</h1>')
-
-def contact_page(request):
-    return HttpResponse('<h1>Contact Page</h1>')
-
-# Example: Render any type of data
-def example_page(request):
-    context = {'title': 'A text file to test rendering an type of data'}
-    template_name = "text_file.txt"
-    template_obj = get_template(template_name)
-    rendered_string = template_obj.render(context)
-    return HttpResponse(rendered_string)
-
-
 # Create your views here.
 # def blog_post_detail_page(request):
 #     obj = BlogPost.objects.get(id=1) # query -> database -> data -> django renders it
